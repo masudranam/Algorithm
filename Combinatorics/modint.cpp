7a4eaf1c1002d721cc4b913abf7f2958f9d196b3
Md Masud Rana <83323510+masudranam@users.noreply.github.com>
2023-05-04 17:37:11 +0600

    Update modint.cpp

--- Combinatorics/modint.cpp
+++ Combinatorics/modint.cpp
@@ -46,3 +46,113 @@ struct Combi{
 };
 Combi C(N);
  
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define print(a) for(auto x:a)cout<<x<<' ';cout<<'\n';
+#define debug(x) cout<<#x<<" "<<x<<endl
+#define all(a) (a).begin(),(a).end()
+#define sz(a) (int)(a.size())
+#define int   long long int
+#define endl '\n'
+#define ar array
+
+const int MOD = 1e9 + 7;
+const int N = 2e6 + 10;
+
+template<int MOD>
+struct ModInt {
+  unsigned x;
+  ModInt() : x(0) { }
+  ModInt(signed sig) : x(sig) {  }
+  ModInt(signed long long sig) : x(sig%MOD) { }
+  int get() const { return (int)x; }
+  ModInt pow(int p) { ModInt res = 1, a = *this; while (p) { if (p & 1) res *= a; a *= a; p >>= 1; } return res; }
+ 
+  ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+  ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+  ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return (*this) *= that.pow(MOD - 2); }
+ 
+  ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+  bool operator<(ModInt that) const { return x < that.x; }
+  friend ostream& operator<<(ostream &os, ModInt a) { os << a.x; return os; }
+};
+typedef ModInt<998244353> mint;
+
+
+
+template<int m>
+struct modint {
+    unsigned x;
+ 
+    modint() : x(0) {}
+    modint(long long arg) : x((arg %= m) < 0 ? arg + m : arg) {}
+ 
+    modint operator+ (const modint& b) const {
+        auto t = *this; t.x = (t.x += b.x) >= m ? t.x-m : t.x; return t;
+    }
+ 
+    modint operator- (const modint& b) const {
+        auto t = *this; t.x = (t.x += m-b.x) >= m ? t.x-m : t.x; return t;
+    }
+ 
+    modint operator* (const modint& b) const {
+        auto t = *this; t.x = ll(t.x) * b.x % m; return t;
+    }
+ 
+    modint operator/ (const modint& b) const {
+        return *this * b.inv();
+    }
+ 
+    modint operator-() const {
+        auto t = *this; t.x = x ? m-x : 0; return t; 
+    }
+ 
+    template<class T> modint operator^ (T n) const {
+        modint t;
+        if (n == 0) return t.x = 1, t;
+        if (n == 1) return t.x = x, t;
+        t = *this ^ (n >> 1); t *= t;
+        return n & 1 ? *this * t : t;
+    }
+ 
+    modint& operator+= (const modint& b) { return *this = *this + b; }
+    modint& operator-= (const modint& b) { return *this = *this - b; }
+    modint& operator*= (const modint& b) { return *this = *this * b; }
+    modint& operator/= (const modint& b) { return *this = *this / b; }
+    modint& operator++ () { if (++x == m) x = 0; return *this; }
+    modint& operator-- () { if (x-- == 0) x = m-1; return *this; }
+    modint operator++ (int) { modint tmp = *this; ++*this; return tmp; }
+    modint operator-- (int) { modint tmp = *this; --*this; return tmp; }
+    bool operator== (const modint& other) const { return x == other.x; }
+    bool operator!= (const modint& other) const { return x != other.x; }
+    int operator() () const { return x; }
+    template<class T> modint operator^= (T n) { return *this = *this ^ n; }
+    modint inv() const { return *this ^ (m-2); }
+};
+ 
+const int mod = 1000 * 1000 * 1000 + 7;
+typedef modint<mod> mint;
+ 
+void solve() {
+    mint n=2;
+   cout<<n.pow(9)<<endl;
+                
+}
+
+signed main() {
+   ios_base::sync_with_stdio (0);
+   cin.tie (0);
+
+   int t = 1;  //cin >> t;
+   for (int tc = 1; tc <= t; tc++) {
+      //cout << "Case " << tc << ": \n";
+      solve();
+   }
+   return 0;
+}
